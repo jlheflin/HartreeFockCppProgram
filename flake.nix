2507feaf39d79340de46d0e815f4ab687f6aea79
Jacob Heflin <btheflin@gmail.com>
2025-06-26 13:08:44 -0500

    added nix implementation for building

--- flake.nix
+++ flake.nix
@@ -1,11 +1,49 @@
 {
-  inputs.nixpkgs.url = "github.com:nixos/nixpkgs";
+  description = "Hartree-Fock C++ program";
+
+  inputs.nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+
   outputs = {self, nixpkgs, ...}:
     let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
     in
     {
-      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+      packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "program";
+          version = "1.0.0";
+
+          src = ./.;
+          nativeBuildInputs = [ pkgs.cmake ];
+          buildInputs = [ pkgs.boost pkgs.eigen ];
+
+          cmakeFlags = [ "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}" ];
+
+          installPhase = ''
+          runHook preInstall
+          cmake --install .
+          runHook postInstall
+          '';
+        };
+      });
+      
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.boost
+              pkgs.eigen
+              pkgs.cmake
+            ];
+            shellHook = ''
+              echo "Welcome to dev shell!"
+            '';
+          };
+        });
     };
 }
