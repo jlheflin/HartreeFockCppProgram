642b982ce68b0b4a4bf9e4e5b59b99384f72858d
Jacob Heflin <btheflin@gmail.com>
2025-06-29 12:52:41 -0500

    removed for loop, added only a single basis and file to run, basis can now be selected via cli opts

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,13 @@ std::string to_string_ten(const Eigen::Tensor<double, 4>& ten) {
 }
 
 int main(int argc, char* argv[]) {
+  std::string  xyz_file, basis;
   try {
     cxxopts::Options options("program", "Hartree Fock Toy Code");
     options.add_options()
       ("l,log-level", "Set logging level [debug, info]", cxxopts::value<std::string>()->default_value("info"))
+      ("f,xyz-file", "XYZ File to use", cxxopts::value<std::string>()->default_value("h2.xyz"))
+      ("b,basis", "basis set to use", cxxopts::value<std::string>()->default_value("sto-3g"))
       ("h,help", "Print usage");
     auto result = options.parse(argc, argv);
 
@@ -29,6 +32,9 @@ int main(int argc, char* argv[]) {
     }
 
     std::string log_level = result["log-level"].as<std::string>();
+    xyz_file = result["xyz-file"].as<std::string>();
+    basis = result["basis"].as<std::string>();
+    
     if (log_level != "debug" && log_level != "info") {
       spdlog::error("Unknown logging level: {}", log_level);
       return 1;
@@ -46,42 +52,36 @@ int main(int argc, char* argv[]) {
   
   spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%5l%$] %v");
 
+  std::ifstream xyz(xyz_file);
+  spdlog::info("Using XYZ File: {}", xyz_file);
+  if (!xyz) {
+    spdlog::error("XYZ File not found: {}", xyz_file);
+    return 1;
+  }
+
   libint2::initialize();
-
-  std::vector<std::string> basis_sets;
-  basis_sets.push_back("sto-3g");
-  basis_sets.push_back("sto-6g");
-  basis_sets.push_back("3-21g");
-  basis_sets.push_back("6-31g");
-  basis_sets.push_back("6-311gss");
-  // basis_sets.push_back("cc-pvqz");
-  // basis_sets.push_back("cc-pv6z");
-
-
-  std::ifstream xyz("h2.xyz");
   std::vector<libint2::Atom> atoms = libint2::read_dotxyz(xyz);
 
-  for (std::string basis : basis_sets) {
-    spdlog::info("Basis: {}", basis);
-    libint2::BasisSet obs(basis, atoms);
-    auto S = overlap(obs);
-    spdlog::trace("\nOverlap Matrix:\n{}", to_string_mat(S));
-    auto T = kinetic(obs);
-    spdlog::trace("\nKinetic Matrix:\n{}", to_string_mat(T));
-    auto V_ne = electron_nuclear_attraction(obs, atoms);
-    spdlog::trace("\nV_ne Matrix:\n{}", to_string_mat(V_ne));
-    auto V_ee = electron_electron_repulsion(obs);
-    spdlog::trace("\nV_ee Matrix:\n{}", to_string_ten(V_ee));
-    auto E_NN = nuclear_nuclear_repulsion_energy(atoms);
-    spdlog::debug("E_nn Value: {}", E_NN);
-    auto molecular_terms = std::make_tuple(S, T, V_ne, V_ee);
-    auto scf_parameters = std::make_tuple(1e-8, 50);
-    auto electronic_energy = scf_cycle(molecular_terms, scf_parameters, obs, atoms);
-    spdlog::debug("Electronic Energy: {}", electronic_energy);
-    auto total_energy = electronic_energy + E_NN;
+  spdlog::info("Basis: {}", basis);
+  libint2::BasisSet obs(basis, atoms);
+  auto S = overlap(obs);
+  spdlog::trace("\nOverlap Matrix:\n{}", to_string_mat(S));
+  auto T = kinetic(obs);
+  spdlog::trace("\nKinetic Matrix:\n{}", to_string_mat(T));
+  auto V_ne = electron_nuclear_attraction(obs, atoms);
+  spdlog::trace("\nV_ne Matrix:\n{}", to_string_mat(V_ne));
+  auto V_ee = electron_electron_repulsion(obs);
+  spdlog::trace("\nV_ee Matrix:\n{}", to_string_ten(V_ee));
+  auto E_NN = nuclear_nuclear_repulsion_energy(atoms);
+  spdlog::debug("E_nn Value: {}", E_NN);
+  auto molecular_terms = std::make_tuple(S, T, V_ne, V_ee);
+  auto scf_parameters = std::make_tuple(1e-8, 50);
+  auto electronic_energy = scf_cycle(molecular_terms, scf_parameters, obs, atoms);
+  spdlog::debug("Electronic Energy: {}", electronic_energy);
+  auto total_energy = electronic_energy + E_NN;
 
-    spdlog::info("Total energy: {:.16f}", total_energy);
-  }
+  spdlog::info("Total energy: {:.16f}", total_energy);
+
   libint2::finalize();
   return 0;
 }
