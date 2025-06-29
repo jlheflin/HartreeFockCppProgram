4e2e9de67427e47c5310ca0b1fb634d2be710073
Jacob Heflin <btheflin@gmail.com>
2025-06-29 09:18:06 -0500

    separated out functions.cpp and cleaned up other files

---
+++ include/density.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11//Tensor>
+
+using tensor4d = Eigen::Tensor<double, 4>;
+using matrix2d = Eigen::MatrixXd;
+
+matrix2d 
+compute_density_matrix(const matrix2d  &mos, int n_occ);
+
+matrix2d compute_G(matrix2d  dens_mat, tensor4d Vee);
